234987917f9fe587fa45e9278809836f13cdac2b
Wilson Vargas <wilsonvargas_6@outlook.com>
2019-05-13 13:00:08 -0500

    Updated to Mvvm pattern

--- QRManager/QRManager/QRManager/Views/QRGeneratorPage.xaml.cs
+++ QRManager/QRManager/QRManager/Views/QRGeneratorPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using QRManager.ViewModels;
+using System;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -12,37 +13,7 @@ namespace QRManager.Views
         public QRGeneratorPage()
         {
             InitializeComponent();
-        }
-
-        private ZXingBarcodeImageView barcode;
-
-        private void Button_Clicked(object sender, EventArgs e)
-        {
-            try
-            {
-                if (contentEntry.Text != string.Empty)
-                {
-                    barcode = new ZXingBarcodeImageView
-                    {
-                        HorizontalOptions = LayoutOptions.FillAndExpand,
-                        VerticalOptions = LayoutOptions.FillAndExpand,
-                    };
-                    barcode.BarcodeFormat = ZXing.BarcodeFormat.QR_CODE;
-                    barcode.BarcodeOptions.Width = 500;
-                    barcode.BarcodeOptions.Height = 500;
-                    barcode.BarcodeValue = contentEntry.Text.Trim();
-                    qrResult.Content = barcode;
-                }
-                else
-                {
-                    DisplayAlert("Alert", "Introduzca el valor que desea convertir código QR", "OK");
-                }
-            }
-            catch (Exception ex)
-            {
-                System.Diagnostics.Debug.WriteLine(ex.ToString());
-                DisplayAlert("Alert", "Introduzca el valor que desea convertir código QR", "OK");
-            }
-        }
+            BindingContext = new QRGeneratorViewModel();
+        }             
     }
 }
